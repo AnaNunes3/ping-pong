e603e55837ecd780e2e119eab4f2ab04a1941880
AnaNunes3 <nunes.silva13@escola.pr.gov.br>
2026-02-27 07:57:13 -0300

    upd

--- pinpong.js
+++ pinpong.js
@@ -20,12 +20,37 @@ let yRaqueteOponente = 150;
 let meusPontos = 0;
 let pontosOponente = 0;
 
+//dificuldade
+
+let dificuldade = "Normal";
+let trilha,ponto,raquetada;
+
+function preload(){
+  trilha = loadSound("trilha.mp3")
+  pontos = loadSound("ponto.mp3")
+  raquetada = loadSound("raquetada.mp3")
+}
+
 function setup(){
   createCanvas(600,400);
+  trilha.loop()
 }
 
 function draw(){
+  background(0);
+  movimentoBolinha();
+  raquetes(xRaquete,yRaquete);
+  raquetes(xRaqueteOponente,yRaqueteOponente);
+  npc(dificuldade)
   
+  if(KeyIsDown(UP_ARROW)){
+    yRaquete-= 10}
+  if(KeyIsDown(DOWN_ARROW)){
+  yRaquete += 10}
+  
+  if(xBolinha -raio < xRaquete + 10 & yBolinha-raio< yRaquete + 90 & yBolinha +raio > yRaquete ){
+    velocidadeXBolinha +=1;
+  }
 }
   
 function movimentoBolinha(){
